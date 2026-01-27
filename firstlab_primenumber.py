6e59083c2fa57d23a4289d93e50a8acfbae35ac7
Aditya Kumar <adityaara7667@gmail.com>
2026-01-27 16:25:06 +0530

    first

---
+++ firstlab_primenumber.py
@@ -0,0 +1,12 @@
+# def check_prime(n):
+#     count = 0
+#     for i in range(1, n + 1):
+#         if n % i == 0:
+#             count += 1
+            
+#     if count == 2:
+#         print("this is a Prime Number")
+#     else:
+#         print("this  is not a Prime Number")
+
+# check_prime(12)
---
+++ power_raise.py
@@ -0,0 +1,8 @@
+# n = int (input("enter your number"))
+# m = int (input("enter power number"))
+
+# print(pow(n,m))
+# --------------------------------------------------------------------------------------
+# def power(m,n):
+#     print(m**n)
+# power(2,3)  
