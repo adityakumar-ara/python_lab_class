49d211e3b15a7ca65ecd6067d7a60417880249a0
Aditya Kumar <adityaara7667@gmail.com>
2026-02-11 10:54:48 +0530

    add more

--- day2_lab/day22.py
+++ day2_lab/day22.py
@@ -1,11 +1,23 @@
+# n = int (input("enter your marks"))
+# if(n>=90):
+#     print("gread A")
+# elif(n>=75 and n<=89):
+#     print("gread B")    
+# elif(n>=60 and n<=74):
+#     print("gread c")       
+# elif(n>=40 and n<=59):
+#     print("gread d")       
+# else:
+#     print("your are fail")
+
 n = int (input("enter your marks"))
 if(n>=90):
     print("gread A")
-elif(n>=75 and n<=89):
+elif(n>=75):
     print("gread B")    
-elif(n>=60 and n<=74):
+elif(n>=60):
     print("gread c")       
-elif(n>=40 and n<=59):
+elif(n>=40):
     print("gread d")       
 else:
     print("your are fail")
