d1196ad9d2e72c412d38b900c38e82ac104338bb
Aditya Kumar <adityaara7667@gmail.com>
2026-02-11 10:49:22 +0530

    add more

---
+++ day2_lab/day22.py
@@ -0,0 +1,11 @@
+n = int (input("enter your marks"))
+if(n>=90):
+    print("gread A")
+elif(n>=75 and n<=89):
+    print("gread B")    
+elif(n>=60 and n<=74):
+    print("gread c")       
+elif(n>=40 and n<=59):
+    print("gread d")       
+else:
+    print("your are fail")
