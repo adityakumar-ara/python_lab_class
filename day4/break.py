31e96be3352a2ba0c9ab297cec01d8dd64579065
Aditya Kumar <adityaara7667@gmail.com>
2026-02-16 19:08:54 +0530

    add more

--- day4/break.py
+++ day4/break.py
@@ -1,4 +1,9 @@
-for i in range(0,10):
-    print(i)
-    if(i==4):
-        break
+# for i in range(0,10):
+#     print(i)
+#     if(i==4):
+#         break
+
+# for i in range(0,11):
+#     print(i)
+# else:
+#     print("done")    
