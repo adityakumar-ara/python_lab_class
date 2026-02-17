c894db63abed8c1c37e494c6ab5c7bcf271615af
Aditya Kumar <adityaara7667@gmail.com>
2026-02-17 15:59:00 +0530

    add more

--- day5/checkprime.py
+++ day5/checkprime.py
@@ -1,3 +1,14 @@
 # n = int (input("enter your number:"))
-# count = 0
-# if (n%2==)
+def checkprime(n):
+ count = 0
+ for i in range(1,n+1):
+   if (n%i==0):
+    count+=1
+ if(count==2):
+   print("this is prime number:")   
+ else:
+   print("not prime") 
+checkprime(6) 
+   
+
+# checkprime(5)   
