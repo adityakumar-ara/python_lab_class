5d8e9c0055fa7db6a0cea7dd299e34a4ab9f9e74
Aditya Kumar <adityaara7667@gmail.com>
2026-02-20 14:58:08 +0530

    add more

--- day6/formatestring.py
+++
@@ -1,6 +0,0 @@
-name = "aditya kumar"
-age = 20
-marks = 85.95
-print("name:%s"%name)
-print("age:%d"%age)
-print("marks:%.2f"%marks)
---
+++ day6/formatstring.py
@@ -0,0 +1,29 @@
+# name = "aditya kumar"
+# age = 20
+# marks = 85.95
+# print("name:%s"%name)
+# print("age:%d"%age)
+# print("marks:%.2f"%marks)
+
+
+# name = "aditya kumar"
+# age = 20
+# print("i'm {} i'm {} years old.".format(name,age))
+
+# # -----------4.index formatting----------
+# name = "aditya kumar"
+# age = 20
+# print("i'm {0} i'm {1} years old.".format(name,age))
+
+# -----------5.Number formatting-----------
+
+# num = 1234.5678
+# print("two decimal:{:.2f}".format(num))
+
+
+# -----------6.string alignment-----------
+
+# name= "aditya"
+# print("left-align:{:<10}".format(name))
+# print("Right-align:{:>10}".format(name))
+# print("center-align:{:^10}".format(name))
