0c273609cd833927b8d25d5b3a7e2b43528070e1
Aditya Kumar <adityaara7667@gmail.com>
2026-02-23 10:58:30 +0530

    add more

---
+++ day7/len.py
@@ -0,0 +1,17 @@
+# name = "aditya"
+# # _____________LENTH function_______________________________________________
+# print(len(name))
+# # _____________raplace function_______________________________________________
+
+# print(name.replace("a","A"))
+# # _____________upper function_______________________________________________
+
+# print(str.upper(name))
+
+# # _____________lower function_______________________________________________
+# print(str.lower(name))
+
+# language = "python"
+# slace = language[-3:]
+# print(slace)
+
