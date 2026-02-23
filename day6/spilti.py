84f6afd4b61d19cf37c59f9d262a4c4a1de160a1
Aditya Kumar <adityaara7667@gmail.com>
2026-02-23 10:20:03 +0530

    add more

--- day6/spilti.py
+++ day6/spilti.py
@@ -1,3 +1,13 @@
-text = " Ram,is,good"
-new_text = text.split(",")
+# text = " Ram,is,good"
+# new_text = text.split(",")
+# print(new_text)
+
+
+
+text = " Ram,is,god"
+
+words_list = text.split(",")
+
+new_text = " ".join(words_list)
+
 print(new_text)
---
+++ day7/he.py
@@ -0,0 +1,8 @@
+# wap to print vowel count
+vowel ="aeiouAEIOU"
+count = 0
+name = "aditya"
+for text in name:
+    if text in vowel:
+        count+=1
+print(count)        
