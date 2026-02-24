0d0592e5aa0f848a8a01559e90252ea0c8df1185
Aditya Kumar <adityaara7667@gmail.com>
2026-02-24 13:46:34 +0530

    add more

---
+++ pythom_lab_file/oddEven.py
@@ -0,0 +1,8 @@
+def oddeven(n):
+    if n % 2 == 0:
+       return True
+    else :
+        return False
+n = int(input("enter any number")) 
+result = oddeven(n)
+print(result)      
---
+++ pythom_lab_file/raisetopower.py
@@ -0,0 +1,6 @@
+def power(a,b):
+    return a**b
+a = int(input("enter saise number:"))
+b = int(input("enter power number:"))
+result = power(a,b)
+print(result)
