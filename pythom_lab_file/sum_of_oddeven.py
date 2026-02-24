cf94964f4f18a1779a589ecb06d499a534d3c076
Aditya Kumar <adityaara7667@gmail.com>
2026-02-24 15:35:21 +0530

    add more

---
+++ pythom_lab_file/sum_of_oddeven.py
@@ -0,0 +1,11 @@
+# list = [1,2,3,4,5,6,7,8]
+# evensum = 0
+# oddnsum = 0
+# for i in list:
+#     if i % 2:
+#        evensum=+i
+#     else:
+#         oddnsum=+i 
+
+# print(evensum)        
+# print(oddnsum)        
