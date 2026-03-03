e631ae0ac6ec78c606a9e3548e7ebf3cab6713be
Aditya Kumar <adityaara7667@gmail.com>
2026-03-03 19:40:30 +0530

    add more

--- pythom_lab_file/sum_of_oddeven.py
+++ pythom_lab_file/sum_of_oddeven.py
@@ -10,19 +10,19 @@
 # print(evensum)        
 # print(oddnsum)  
     
-list = []
-evensum = 0
-oddsum = 0
-n = int(input("enter number"))
-for i in range (1,n+1):
-    m = int(input("enter elements"))
-    list.append(m)    
-print(list) 
+# list = []
+# evensum = 0
+# oddsum = 0
+# n = int(input("enter number"))
+# for i in range (1,n+1):
+#     m = int(input("enter elements"))
+#     list.append(m)    
+# print(list) 
 
-for i in list:
-    if i % 2==0:
-        evensum+=i
-    else:    
-        oddsum+=i
-print(evensum)        
-print(oddsum)        
+# for i in list:
+#     if i % 2==0:
+#         evensum+=i
+#     else:    
+#         oddsum+=i 
+# print(evensum)        
+# print(oddsum)        
