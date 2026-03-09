a83271bfdb5ccdd9ba8b6d7113a2a51dab76a4ce
Aditya Kumar <adityaara7667@gmail.com>
2026-03-09 10:39:39 +0530

    add more

--- python_class/day6/starpatern.py
+++ python_class/day6/starpatern.py
@@ -1,4 +1,5 @@
-for i in range(0,4):
-    for j in range(i):
-        print("* ", end="")
-    print()    
+# for i in range(0,4):
+#     for j in range(i):
+#         print("* ", end="")
+#     print()    
+
