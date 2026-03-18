ede28b96c0e3b2d352c5d740db82b1ec9e86086c
Aditya Kumar <adityaara7667@gmail.com>
2026-03-18 18:27:44 +0530

    add more

--- python_class/day9/armstrong.py
+++ python_class/day9/armstrong.py
@@ -1,12 +1,25 @@
-num = int(input("Ek number enter karein: "))
+# num = int(input("Ek number enter karein: "))
+# order = len(str(num))
+# sum = 0
+# temp = num
+# while temp > 0:
+#     digit = temp % 10
+#     sum += digit ** order
+#     temp //= 10
+# if num == sum:
+#     print(f"{num} ek Armstrong number hai!")
+# else:
+#     print(f"{num} Armstrong number nahi hai.")
+
+num = int(input("Enter number"))
 order = len(str(num))
 sum = 0
 temp = num
 while temp > 0:
-    digit = temp % 10
-    sum += digit ** order
-    temp //= 10
+    r = temp%10
+    sum = sum+(r**order)
+    temp //=10
 if num == sum:
-    print(f"{num} ek Armstrong number hai!")
+    print("this is armstrong number")    
 else:
-    print(f"{num} Armstrong number nahi hai.")
+    print("not armstong number")    
