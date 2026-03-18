0ffb3337be7ff9dd65f88ac78920ee711b657416
Aditya Kumar <adityaara7667@gmail.com>
2026-03-18 18:44:44 +0530

    add more

--- python_class/day9/palidrom.py
+++ python_class/day9/palidrom.py
@@ -1,7 +1,14 @@
+# num = input("enter number")
+# num = num.casefold()
+# a = reversed(num)
+# if list(num) ==list(a):
+#     print("hello")
+# else:
+#     print("bye")    
+
 num = input("enter number")
-num = num.casefold()
-a = reversed(num)
-if list(num) ==list(a):
-    print("hello")
+
+if num == num[::-1]:
+    print("Palindrome")
 else:
-    print("bye")    
+    print("Not Palindrome")
