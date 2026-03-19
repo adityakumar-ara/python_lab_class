a955a2f819782e76b440128a38871d09ce18588d
Aditya Kumar <adityaara7667@gmail.com>
2026-03-19 13:14:59 +0530

    update

--- python_class/day7/vowel.py
+++ python_class/day7/vowel.py
@@ -1,8 +1,8 @@
 # wap to print vowel count
-vowel ="aeiouAEIOU"
+vowel ="AEIOU"
 count = 0
 name = "aditya"
-for text in name:
+for text in name.upper():
     if text in vowel:
         count+=1
 print(count)        
