fbc48576fb8e82b629df82dfdb6dad25bba3630c
Aditya Kumar <adityaara7667@gmail.com>
2026-03-20 21:59:47 +0530

    add more

---
+++ python_class/day9/game.py
@@ -0,0 +1,8 @@
+num = 50
+while(1):
+    u_num = int(input("Chose a number"))
+    if u_num == num:
+          print("wow! you are win")
+          break
+    else:
+         print("Your number is wrong Try again:")
