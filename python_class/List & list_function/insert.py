acc2a357663d44fbd4b7096f4f57bac86d97c93f
Aditya Kumar <adityaara7667@gmail.com>
2026-03-21 10:56:09 +0530

    add more

---
+++ python_class/List & list_function/insert.py
@@ -0,0 +1,9 @@
+# insert(index, item): Adds an item at a specific position (index) that you choose.
+
+# num = [22,33,44,66]
+# num.insert(3,55)
+# print(num)
+
+# name_list = ['aditya','kunal','abhishek','karan']
+# name_list.insert(0,'Gulshan')
+# print(name_list)
