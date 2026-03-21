51fd8f673176a5dd507c1d939c6ade5f1f0e48e9
Aditya Kumar <adityaara7667@gmail.com>
2026-03-21 10:57:01 +0530

    add more

---
+++ python_class/operator/apend.py
@@ -0,0 +1,8 @@
+# append(item): Adds a single item to the very end of the list.
+# my_list =[1,2,3,4]
+# my_list.append(5)
+# # print(my_list)
+
+# names_list = ['aditya','naveen','raju','kunal']
+# names_list.append('Gulshan')
+# print(names_list)
