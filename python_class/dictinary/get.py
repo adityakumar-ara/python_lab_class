b9a67dac9ccc7a6a1c7353e0cd5f677dae7efb9a
Aditya Kumar <adityaara7667@gmail.com>
2026-03-22 17:05:50 +0530

    add more

---
+++ python_class/dictinary/get.py
@@ -0,0 +1,10 @@
+# get() → returns value using key
+my_dict = {
+    "name":"aditya",
+    "age" : 19,
+    "village" :"hardiya"
+}
+print("name",my_dict.get("name"))
+print("age",my_dict.get("age"))
+print("village",my_dict.get("village"))
+print("state",my_dict.get("state"))  #return none
