9b497b7e74a422e5d2934357c0f79c4466a9567b
Aditya Kumar <adityaara7667@gmail.com>
2026-04-08 10:54:55 +0530

    add more

--- python_class/List & list_function/loop_list.py
+++ python_class/List & list_function/loop_list.py
@@ -1,8 +1,17 @@
-my_list = []
-n = int(input("enter number of element"))
+# my_list = []
+# n = int(input("enter number of element"))
 
-for i in range(0,n):
-    element = int(input("enter element"))
-    my_list.append(element)
+# for i in range(0,n):
+#     element = int(input("enter element"))
+#     my_list.append(element)
 
-print(my_list)
+# print(my_list)
+
+
+lst = [1,2,3]
+lst.append(4)
+print(lst)
+print(max(lst))
+print(min(lst))
+lst.remove(2)
+print(lst)
