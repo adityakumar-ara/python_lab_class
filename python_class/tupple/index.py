88665cb726814e897048d6a69e08c666fe047050
Aditya Kumar <adityaara7667@gmail.com>
2026-04-10 14:21:50 +0530

    add more

--- python_class/tupple/index.py
+++ python_class/tupple/index.py
@@ -1,2 +1,9 @@
-my_tupple = (1,2,3,4,5,5,5,5,5,5,6,7,7,7)
-print(my_tupple.index(5))
+# my_tupple = (1,2,3,4,5,5,5,5,5,5,6,7,7,7)
+# print(my_tupple.index(5))
+
+# my_tuple = (1,2,3,4)
+# print(my_tuple.index(2))
+
+my_tuple = (1,2,3,3,4,45)
+print(my_tuple.index(3))
+print(my_tuple.count(3))
