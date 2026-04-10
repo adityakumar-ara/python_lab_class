699efe06e965927b44ff914acb45a6b5c1762fee
Aditya Kumar <adityaara7667@gmail.com>
2026-04-10 14:32:47 +0530

    add more

--- python_class/dictinary/simple.py
+++ python_class/dictinary/simple.py
@@ -1,5 +1,13 @@
+# my_dict = {"name" : "aditya",
+#            "age" : 20,
+#            "village" : "khada tola"}
+
+# print(my_dict)
+
 my_dict = {"name" : "aditya",
            "age" : 20,
            "village" : "khada tola"}
 
+print(my_dict)
+my_dict.update({"post":"hhhhh"})
 print(my_dict)
