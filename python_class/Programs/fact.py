c1d53ab94d7c1af4cd5ac3ffdef1431a2fc57bbe
Aditya Kumar <adityaara7667@gmail.com>
2026-04-17 13:50:03 +0530

    add more

---
+++ python_class/Programs/fact.py
@@ -0,0 +1,17 @@
+# def fact(n):
+#     if n == 1 or n ==0:
+#         return 1
+#     else:
+#         return n*fact(n-1)
+# print(fact(5))  
+  
+# ---------------------------------------------------------------------------
+
+# def fact(n):
+#     fact = 1
+#     for i in range(1,n+1):
+#         fact*=i
+#     return fact    
+# num = 5    
+# result = fact(num)
+# print(result)
