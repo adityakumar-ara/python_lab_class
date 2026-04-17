8f3877a5fc503ebbe268725206043396bfa9d46b
Aditya Kumar <adityaara7667@gmail.com>
2026-04-17 13:49:21 +0530

    add more

---
+++ python_class/List & list_function/MAP/basic.py
@@ -0,0 +1,36 @@
+# lst = [2,4,6,8]
+# assest = len(lst)
+
+# if assest > 0:
+#     square = list(map(lambda x: x**2, lst))
+#     print("square", square)
+# ---------------------------------------------------------------
+# lst = [2,4,6,8]
+# lis = []
+# assest = len(lst)
+# if assest > 0:
+#  for i in lst:
+#     sqare = pow(i,2)
+#     lis.append(sqare)
+# print(lis)    
+
+# -----------------------------------
+
+# lst = [2,4,6,8]
+# lis = []
+# assest = len(lst)
+# if assest > 0:
+#  for i in lst:
+#     sqare = i**2
+#     lis.append(sqare)
+# print(lis)    
+
+# ------------------------------------------------------------------------------
+
+# BEST SHORTCUT
+
+# lst = [2,4,6,8]
+# result = [i**2 for i in lst]
+# print(result)
+
+    
